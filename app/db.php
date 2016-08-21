483a6cdef3d02b119dae419493d70197bbf8d40b
Jeremy Evans <openspacehexagon@gmail.com>
2016-08-21 17:38:05 -0400

    removing old php code

--- app/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-	// db.php
-	//
-	// required by the backend and almost all of the front end php files
-	$connection = mysqli_connect("localhost", "user", "password", "data_spacehexagon");
-	if (!$connection) {
-    	echo 'Connect Error (' . mysqli_connect_errno() . ') ' . mysqli_connect_error();
-    }
-?>
